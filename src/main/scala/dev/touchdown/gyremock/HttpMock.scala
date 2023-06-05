7c979d9360b55afa5bdbbafb6905b1b652b1460f
Weiwen Zhang <weiwen@weiwen.org>
2023-06-05 10:22:14 +0000

    improve server side streaming support

--- src/main/scala/dev/touchdown/gyremock/HttpMock.scala
+++ src/main/scala/dev/touchdown/gyremock/HttpMock.scala
@@ -7,13 +7,15 @@ import java.net.http.HttpResponse
 
 import scala.compat.java8.FutureConverters._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.util.{Success, Try}
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.common.Slf4jNotifier
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer
 import com.typesafe.scalalogging.StrictLogging
+import org.json4s.JsonAST
+import org.json4s.jackson.JsonMethods
 import scalapb.{GeneratedMessage, GeneratedMessageCompanion}
 import scalapb.json4s.{Parser, Printer}
 
@@ -52,6 +54,37 @@ class HttpMock(settings: WiremockSettings, jsPrinter: Printer, jsParser: Parser)
             m => logger.info("resp proto:\n{}", m.toProtoString)
           )
           resp.get
+      }
+  }
+
+  def sendStream[I <: GeneratedMessage, O <: GeneratedMessage: GeneratedMessageCompanion](message: I, path: String)(
+    implicit ec: ExecutionContext
+  ): Future[List[O]] = {
+    val json = jsPrinter.print(message)
+    logger.info("received raw message:\n{}\njson:\n{}", message.toProtoString, json)
+    val request = HttpRequest.newBuilder
+      .uri(URI.create(targetBaseUrl + path))
+      .POST(HttpRequest.BodyPublishers.ofString(json))
+      .build
+    HttpClient.newHttpClient
+      .sendAsync(request, HttpResponse.BodyHandlers.ofString)
+      .toScala
+      .map {
+        r =>
+          val js = JsonMethods
+            .parse(r.body()) match {
+            case JsonAST.JArray(arr) => arr
+            case j                   => List(j)
+          }
+          js.map {
+            jValue =>
+              val resp = Try(jsParser.fromJson[O](jValue))
+              resp.fold(
+                ex => logger.error("failed to parse into protos", ex),
+                m => logger.info("resp proto:\n{}", m.toProtoString)
+              )
+              resp.get
+          }
       }
   }
 }

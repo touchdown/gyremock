495249f9280356e3fd73fab17769ff97f3f8180a
Tony Zhao <tzhao@tubi.tv>
2023-12-26 23:07:01 -0600

    drop scala 2.12 runtime support

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
     val scala213 = "2.13.12"
 
     // the order in the list is important because the head will be considered the default.
-    val CrossScalaForLib: Seq[String] = Seq(scala213, scala212)
+    val CrossScalaForLib: Seq[String] = Seq(scala213)
     val CrossScalaForPlugin: Seq[String] = Seq(scala212)
   }
 }

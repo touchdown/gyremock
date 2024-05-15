ed091e6ac980ce9e4364b145ec4ff460b387f796
Qiwen Hu <qiwenhu@tubi.tv>
2024-05-15 17:22:32 +0800

    Change run() return Future[Http.Serverbind]

--- runtime/src/main/scala/io/github/touchdown/gyremock/GyremockServer.scala
+++ runtime/src/main/scala/io/github/touchdown/gyremock/GyremockServer.scala
@@ -1,9 +1,5 @@
 package io.github.touchdown.gyremock
 
-import scala.collection.immutable
-import scala.concurrent.Future
-import scala.util.{Failure, Success, Try}
-
 import akka.Done
 import akka.actor.{ActorSystem, CoordinatedShutdown}
 import akka.grpc.scaladsl.{ServerReflection, ServiceHandler}
@@ -13,6 +9,10 @@ import com.github.tomakehurst.wiremock.common.Slf4jNotifier
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 import com.typesafe.scalalogging.StrictLogging
 import scalapb.json4s.{Parser, Printer}
+
+import scala.collection.immutable
+import scala.concurrent.Future
+import scala.util.Try
 
 class GyremockServer(settings: GyremockSettings, services: immutable.Seq[Service])(implicit sys: ActorSystem)
     extends StrictLogging {
@@ -45,16 +45,9 @@ class GyremockServer(settings: GyremockSettings, services: immutable.Seq[Service
   )
 
   /** binds to designated host and port, and registers to shutdown if bind successful */
-  def run(): Unit = {
+  def run(): Future[Http.ServerBinding] = {
     Http()
       .newServerAt(interface = settings.host, port = settings.port)
       .bind(handlers)
-      .onComplete {
-        case Success(binding) =>
-          logger.info(s"gRPC mock server bound to: ${binding.localAddress}")
-          binding.addToCoordinatedShutdown(settings.stopTimeout)
-        case Failure(exception) =>
-          logger.error(s"exception in binding to ${settings.host}:${settings.port}", exception)
-      }(sys.dispatcher)
   }
 }
--- runtime/src/main/scala/io/github/touchdown/gyremock/GyremockSettings.scala
+++ runtime/src/main/scala/io/github/touchdown/gyremock/GyremockSettings.scala
@@ -1,18 +1,26 @@
 package io.github.touchdown.gyremock
 
-import scala.concurrent.duration.FiniteDuration
-import scala.jdk.DurationConverters._
-
 import com.typesafe.config.Config
+import io.github.touchdown.gyremock.GyremockSettings.{dynamicPort, localhost}
 
 object GyremockSettings {
+  private val localhost = "127.0.0.1"
+
+  // when we set port = 0, that means we will use dynamic port
+  private val dynamicPort = 0
+
   def apply(config: Config): GyremockSettings =
     GyremockSettings(
       host = config.getString("host"),
       port = config.getInt("port"),
-      stopTimeout = config.getDuration("stop-timeout").toScala,
       wiremockBaseUrl = Option.when(config.hasPath("wiremock-base-url"))(config.getString("wiremock-base-url"))
     )
 }
 
-case class GyremockSettings(host: String, port: Int, stopTimeout: FiniteDuration, wiremockBaseUrl: Option[String])
+/**
+ *
+ * @param host default host is localhost
+ * @param port default port is 0; when the port is 0, we will use dynamic port to setup gyremock server
+ * @param wiremockBaseUrl wiremock url since gyremock will ask wiremock to handle request
+ */
+case class GyremockSettings(host: String = localhost, port: Int = dynamicPort, wiremockBaseUrl: Option[String])
